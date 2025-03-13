28ae8a4b6841ca547b28224330623f313ad40f52
giacomorapagnani <g.rapagnani@studenti.unipi.it>
2025-03-13 16:53:46 +0100

    up

--- codes/plot_map/map_focal_mechanisms.py
+++ codes/plot_map/map_focal_mechanisms.py
@@ -50,7 +50,7 @@ fig.grdimage(grid=topo_data, region=region, projection=projection, shading="+a45
 fig.coast(shorelines="1/0.5p,black", resolution="f", water="#EBEBEE")
 
 #   PLOT FOCAL MECHANISM
-filename='catalogue_flegrei_MT_final'             ###CHANGE###  'catalogue_flegrei_MT_final'
+filename='catalogue_flegrei_MT_final_VLP_2_reloc'             ###CHANGE###  'catalogue_flegrei_MT_final'
 events_name=os.path.join(catdir,filename+'.pf')              
 fm_events = model.load_events(events_name)
 
@@ -58,7 +58,7 @@ fm_events = model.load_events(events_name)
 ##########################################
 ############## SWITCH ##############
 ##########################################
-switch_deviatoric=False                                                                                                                               
+switch_deviatoric=True                                                                                                                               
 
 # TRUE if you want timestamps
 ##########################################
