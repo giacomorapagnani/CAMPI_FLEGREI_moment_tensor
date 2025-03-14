029e34de94a8cdd3847c12c2637103ffd26375bd
giacomorapagnani <g.rapagnani@studenti.unipi.it>
2025-03-14 10:55:03 +0100

    up

--- codes/readGrondOut.py
+++ codes/readGrondOut.py
@@ -31,9 +31,9 @@ reportdir=os.path.join(workdir,'report')                                #main re
 #reportdir=os.path.join(workdir,'../FULL_and_DC_report/report')        #cd and full report dir
 catdir=os.path.join(workdir,'CAT')
 
-catname=os.path.join(catdir,'catalogue_flegrei_VLP.pf')    # CHANGE  catalogue_flegrei_VLP catalogue_flegrei_mag_2_5
+catname=os.path.join(catdir,'catalogue_flegrei_mag_2_5.pf')    # CHANGE  catalogue_flegrei_VLP catalogue_flegrei_mag_2_5
 
-new_catalogue_name='catalogue_flegrei_MT_final_VLP_2'                           # CHANGE
+new_catalogue_name='catalogue_flegrei_MT_final_3'                           # CHANGE
 
 refevents=model.load_events(catname)
 
@@ -43,7 +43,7 @@ run_get_grond_results = True
 #######################################
 ############# SWITCH #############
 #######################################
-switch_new_localization=True
+switch_new_localization=False
 
 if run_get_grond_results:
     mttargets = [ev for ev in refevents]
@@ -53,9 +53,10 @@ if run_get_grond_results:
     print('Good events in catalogue:', len(goodmttargets))
     grondevs = []
     #for vrs in ['cmt_devi_XL_final_', 'cmt_devi_L_final_', 'cmt_devi_M_final_','cmt_devi_S_final_','cmt_devi_S_']:                    # main report
+    for vrs in ['cmt_devi_XXL_final_']:                    # main report
     #for vrs in ['cmt_dc_XL_final_', 'cmt_dc_L_final_', 'cmt_dc_M_final_','cmt_dc_S_final_']:                    # dc reports
     #for vrs in ['cmt_full_XL_final_', 'cmt_full_L_final_', 'cmt_full_M_final_','cmt_full_S_final_']:                    # full reports
-    for vrs in ['cmt_LP_oscill_']:
+    #for vrs in ['cmt_LP_oscill_']:
         for ev in goodmttargets:
             targetdir = os.path.join(reportdir, ev.name, vrs + ev.name)
             #if not os.path.isdir(targetdir):
